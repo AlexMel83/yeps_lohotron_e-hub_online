b2da729373ccf0bbc384f34f37c0c5a94b6516c6
AlexMel83 <meleshko.a.n@ukr.net>
2023-10-21 18:45:59 +0300

    add post method

--- backend/src/domain-layer/use-cases/phones.use-case.js
+++ backend/src/domain-layer/use-cases/phones.use-case.js
@@ -2,6 +2,11 @@ const PhoneRepository = require("../../data-layer/phones.repository.js");
 const Phone = require("../entities/phones.entity.js");
 
 module.exports = class PhoneUseCase {
+    mapFields = {
+        id: "id",
+        phone: "phone"
+    }
+
     async getPhones() {
         const phoneRepository = new PhoneRepository();
 
@@ -24,5 +29,46 @@ module.exports = class PhoneUseCase {
         } catch (error) {
             throw error;
         }
+    }
+
+    async addPhone(data) {
+        const phoneRepositary = new PhoneRepository();
+
+        if (!data.fields && !Array.isArray(data.fields) && !data.fields.length) {
+            throw "Data for update not exist";
+        }
+        const fields = data.fields;
+        const isStringChecked = this.checkStringFieldsInsert(fields);
+
+        if (!isStringChecked) {
+            throw "Wrong data";
+        }
+
+        try {
+            const phone = await phoneRepositary.addPhone(this.reduceFields(fields));
+            return phone;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    checkStringFieldsInsert(fields) {
+        const keys = fields.map(val => {
+            return Object.entries(val)[0][0];
+        });
+        const IS_VALID = true;
+
+        for (let i = 0; i < keys.length; i++) {
+            if (!(keys[i]) in this.mapFields) return !IS_VALID;
+        }
+        return IS_VALID;
+    }
+
+    reduceFields(fields) {
+        return fields.reduce((acc, el) => {
+            const entriesEl = Object.entries(el);
+            acc[entriesEl[0][0]] = entriesEl[0][1];
+            return acc;
+        }, {});
     }
 }
