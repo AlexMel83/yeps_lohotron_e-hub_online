b9c6878485678b70b59e849c39dc23e117bb162a
Tatiana Slonovskaya <tatianaslonovskaya@gmail.com>
2023-10-22 02:52:11 +0200

    added html and styles for the review-form

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -1,21 +1,5 @@
-<<<<<<< HEAD
-// validation start
 $(document).ready(function () {
-    $('#phone-input').inputmask('+38 (999) 999-99-99', {
-        placeholder: '+38 (___) ___-__-__',
-    });
-
-
-    $('#phone-input').on('input', function (e) {
-        var value = $(this).val();
-        var cleaned = value.replace(/[^0-9+]/g, '');
-        $(this).val(cleaned);
-    });
-});
-
-// validation end
-=======
-$(document).ready(function () {
+    let phones;
 
     // INPUTMASK +38 (___) ___-__-__
     jQuery(".phone").inputmask({
@@ -26,25 +10,28 @@ $(document).ready(function () {
     //-------get response from api-----
 
     async function getPhones() {
-        let phones;
-        await $.get("http://safetalk.fromavdiivka.city:4040/api/phones", function (data, status) {
-            phones = data;
-        });
-        // let w = $.get('http://safetalk.fromavdiivka.city:4040/api/phones')
-        console.log(phones)
+
+        const response = await $.get("http://safetalk.fromavdiivka.city:4040/api/phones");
+
+        return response;
+
     }
 
     let input = document.querySelector('.phone');
     let findButton = document.getElementById('Search-bar-button');
 
-    findButton.addEventListener('click', function (e) {
+    findButton.addEventListener('click', async function (e) {
         e.preventDefault();
-        getPhones();
-    })
+        const phoneNumbers = await getPhones();
+        let exists = phoneNumbers.some(item => item.phone === input.value);
+        if (exists) {
+            document.location.href = "./number-check.html";
+        } else {
+            console.log('Number not exists');
+        }
+    });
 
 
 
 
 })
-
->>>>>>> main
