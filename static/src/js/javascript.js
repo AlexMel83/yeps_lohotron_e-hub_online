b17c8522401818239dae1bc1f7d915c9255837e5
Tatiana Slonovskaya <tatianaslonovskaya@gmail.com>
2023-11-08 03:19:32 +0100

    added script for the Search-bar-button_mobile

--- static/src/js/javascript.js
+++ static/src/js/javascript.js
@@ -1,108 +1,130 @@
-$(document).ready(function(){
-    $('.header_burger').click(function(event){
-        $('.header_burger, .header_menu, header, .header_icon').toggleClass('active');
-        $('body').toggleClass('lock');   
-    });
+$(document).ready(function () {
+   $('.header_burger').click(function (event) {
+      $('.header_burger, .header_menu, header, .header_icon').toggleClass('active');
+      $('body').toggleClass('lock');
+   });
 });
 
 
-$('.header_link').click(function() {  
-    $('.header_burger, .header_menu, header, .header_icon').removeClass('active');
-    $('body').removeClass('lock');
-  });
+$('.header_link').click(function () {
+   $('.header_burger, .header_menu, header, .header_icon').removeClass('active');
+   $('body').removeClass('lock');
+});
 
 
 $(document).ready(function () {
-    let phones;
+   let phones;
 
-    // INPUTMASK +38 (___) ___-__-__
-    jQuery(".phone").inputmask({
-        mask: "+38 (999) 999-99-99",
-        greedy: false,
-    });
+   // INPUTMASK +38 (___) ___-__-__
+   jQuery(".phone").inputmask({
+      mask: "+38 (999) 999-99-99",
+      greedy: false,
+   });
 
-    //-------get response from api-----
+   //-------get response from api-----
 
-    async function getPhones() {
+   async function getPhones() {
 
-        const response = await $.get("http://safetalk.fromavdiivka.city:4040/api/phones");
+      const response = await $.get("http://safetalk.fromavdiivka.city:4040/api/phones");
 
-        return response;
+      return response;
 
-    }
+   }
 
-    let input = document.querySelector('.phone');
-    let findButton = document.getElementById('Search-bar-button');
+   let input = document.querySelector('.phone');
+   let findButton = document.getElementById('Search-bar-button');
+   let findButtonMobile = document.getElementById('Search-bar-button_mobile');
 
-    if (findButton) {
-        findButton.addEventListener('click', async function (e) {
-            e.preventDefault();
-            const phoneNumbers = await getPhones();
-            let phoneNumber = "+" + input.value.replace(/\D/g, "");
+   if (findButton) {
+      findButton.addEventListener('click', async function (e) {
+         e.preventDefault();
+         const phoneNumbers = await getPhones();
+         let phoneNumber = "+" + input.value.replace(/\D/g, "");
 
-            console.log(phoneNumber);
+         console.log(phoneNumber);
 
-            let exists = phoneNumbers.some(item => item.phone === phoneNumber);
-            if (phoneNumber.length === 13) {
-                if (exists) {
-                    document.location.href = `./number-check.html?${phoneNumber}`;
-                } else {
-                    document.location.href = `./number-check-neutral.html?${phoneNumber}`;
-                }
-            } if (phoneNumber === "+380561233333" || phoneNumber === "+380800505800") {
-                document.location.href = `./number-check-positiv.html?${phoneNumber}`;
-            } else console.log("not correct number")
-        });
-    }
-
-    let phoneNumberText = document.querySelectorAll('.phone-number');
-    var p_url = location.search.substring(1);
-
-    if (phoneNumberText) {
-        for (let i = 0; i < phoneNumberText.length; i++) {
-            phoneNumberText[i].innerHTML = p_url;
-        }
-    }
-
-    async function addPhone() {
-        const requestData = {
-            fields: [
-                { phone: p_url }
-            ]
-        };
-
-        try {
-            const response = await fetch("http://safetalk.fromavdiivka.city:4040/api/phones", {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(requestData)
-            });
-
-            if (response.ok) {
-                console.log("Дані успішно надіслано до API");
+         let exists = phoneNumbers.some(item => item.phone === phoneNumber);
+         if (phoneNumber.length === 13) {
+            if (exists) {
+               document.location.href = `./number-check.html?${phoneNumber}`;
             } else {
-                console.log("Виникла помилка при надсиланні даних до API");
+               document.location.href = `./number-check-neutral.html?${phoneNumber}`;
             }
-        } catch (error) {
-            console.log("Сталася помилка під час виконання POST-запиту:", error);
-        }
-    }
+         } if (phoneNumber === "+380561233333" || phoneNumber === "+380800505800") {
+            document.location.href = `./number-check-positiv.html?${phoneNumber}`;
+         } else console.log("not correct number")
+      });
+   }
 
-    let addButton = document.getElementById('addPhone');
+   if (findButtonMobile) {
+      findButtonMobile.addEventListener('click', async function (e) {
+         e.preventDefault();
+         const phoneNumbers = await getPhones();
+         let phoneNumber = "+" + input.value.replace(/\D/g, "");
 
-    if (addButton) {
-        addButton.addEventListener('click', async function (e) {
-            e.preventDefault();
-            if (p_url.length === 13) {
-                const phoneNumber = await addPhone();
-                console.log(phoneNumber)
+         console.log(phoneNumber);
+
+         let exists = phoneNumbers.some(item => item.phone === phoneNumber);
+         if (phoneNumber.length === 13) {
+            if (exists) {
+               document.location.href = `./number-check.html?${phoneNumber}`;
+            } else {
+               document.location.href = `./number-check-neutral.html?${phoneNumber}`;
             }
-        })
+         } if (phoneNumber === "+380561233333" || phoneNumber === "+380800505800") {
+            document.location.href = `./number-check-positiv.html?${phoneNumber}`;
+         } else console.log("not correct number")
+      });
+   }
+
+   let phoneNumberText = document.querySelectorAll('.phone-number');
+   var p_url = location.search.substring(1);
+
+   if (phoneNumberText) {
+      for (let i = 0; i < phoneNumberText.length; i++) {
+         phoneNumberText[i].innerHTML = p_url;
+      }
+   }
+
+   async function addPhone() {
+      const requestData = {
+         fields: [
+            { phone: p_url }
+         ]
+      };
+
+      try {
+         const response = await fetch("http://safetalk.fromavdiivka.city:4040/api/phones", {
+            method: 'POST',
+            headers: {
+               'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(requestData)
+         });
+
+         if (response.ok) {
+            console.log("Дані успішно надіслано до API");
+         } else {
+            console.log("Виникла помилка при надсиланні даних до API");
+         }
+      } catch (error) {
+         console.log("Сталася помилка під час виконання POST-запиту:", error);
+      }
+   }
+
+   let addButton = document.getElementById('addPhone');
+
+   if (addButton) {
+      addButton.addEventListener('click', async function (e) {
+         e.preventDefault();
+         if (p_url.length === 13) {
+            const phoneNumber = await addPhone();
+            console.log(phoneNumber)
+         }
+      })
 
 
-    }
+   }
 
 
 })
@@ -110,5 +132,5 @@ $(document).ready(function () {
 const addPhoneButton = document.getElementById("addPhone");
 
 addPhoneButton.addEventListener("click", function () {
-    addPhoneButton.disabled = true;
+   addPhoneButton.disabled = true;
 });
